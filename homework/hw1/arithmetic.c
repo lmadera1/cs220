4278dc965789891ab1bc8b380b9b254ed0fec32d
Luis Madera <lmadera1@jhu.edu>
2019-09-11 18:58:09 -0400

    updated arithmetic.c for zero division, whitespace, and some malformed expressions

--- homework/hw1/arithmetic.c
+++ homework/hw1/arithmetic.c
@@ -6,17 +6,23 @@ int main(void) {
   char ch;
   float answer;
   printf("Please enter an arithmetic expression using * and / only: \n");
-  scanf("%f",&answer);
-  while(scanf("%c",&ch) == 1){
+  scanf(" %f",&answer);
+  while(scanf(" %c",&ch) == 1){
     if (ch == '/'){
       float next_num;
-      scanf("%f",&next_num);
+      scanf(" %f",&next_num);
+      if (next_num == 0) {
+	printf("\ndivision by zero\n");
+	return 2;
+      }
       answer  = answer / next_num;
-    }
-    if (ch == '*'){
+    } else if (ch == '*') {
       float next_num;
-      scanf("%f",&next_num);
+      scanf(" %f",&next_num);
       answer = answer * next_num;
+    } else {
+      printf("\nmalformed expression\n");
+      return 1;
     }
   }
   printf("\n%f\n",answer);

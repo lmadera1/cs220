df1be2abd3e7c2015e01b1759af0153aca63c411
Luis Madera <lmadera1@jhu.edu>
2019-09-11 21:40:18 -0400

    Updated arithmitic.c with all conditions met

--- homework/hw1/arithmetic.c
+++ homework/hw1/arithmetic.c
@@ -6,19 +6,32 @@ int main(void) {
   char ch;
   float answer;
   printf("Please enter an arithmetic expression using * and / only: \n");
-  scanf(" %f",&answer);
+  if (scanf(" %f",&answer) != 1){
+    printf("\nmalformed expression\n");
+    return 1;
+  }
+  int count = 1;
   while(scanf(" %c",&ch) == 1){
-    if (ch == '/'){
+    count++;
+    if ((ch == '/') && (count % 2 == 0)){
       float next_num;
-      scanf(" %f",&next_num);
-      if (next_num == 0) {
+      if(scanf(" %f",&next_num)!= 1){
+	printf("\nmalformed expression\n");
+	return 1;
+      }
+      count++;
+      if (next_num == 0){
 	printf("\ndivision by zero\n");
 	return 2;
       }
       answer  = answer / next_num;
-    } else if (ch == '*') {
+    } else if ((ch == '*') && (count % 2 == 0)){
       float next_num;
-      scanf(" %f",&next_num);
+      if(scanf(" %f",&next_num) != 1){
+	printf("\nmalformed expression\n");
+	return 1;
+      }
+      count++;
       answer = answer * next_num;
     } else {
       printf("\nmalformed expression\n");
